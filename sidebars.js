8ad2ecaad429b3cfb68f73a320b2cead4f74c769
dllewellyn <danieln.llewellyn@gmail.com>
2022-11-17 20:42:42 +0000

    Replacing

--- sidebars.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    someSidebar: {
-        Overview: ['why'],
-        'Verification' : ['input/started', 'input/verifyurls', 'input/files', 'input/intentverification'],
-        Resilience: ['checks/gettingstarted', 'checks/combinechecks','rootdetection', 'blacklisting', 'oscheck', 'emulatorcheck', 'debugcheck', 'signatureverification', 'installorigin'],
-        Reporting: ['reporting_or_default', 'reporting/safetorunreporting']
-    },
-};
