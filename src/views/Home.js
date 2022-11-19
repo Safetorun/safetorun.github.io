d037b823ab925767c385721de3950629920809b8
dllewellyn <danieln.llewellyn@gmail.com>
2022-11-19 14:47:59 +0000

    Adding sign up form

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,11 +6,16 @@ import Hero from '../components/sections/Hero';
 //import Testimonial from '../components/sections/Testimonial';
 //import Cta from '../components/sections/Cta';
 
+const SignUpForm = () => {
+    return <div className="klaviyo-form-R7jzrH"></div>
+}
+
 const Home = () => {
 
   return (
     <>
       <Hero className="illustration-section-01" />
+        <SignUpForm />
     {/**<FeaturesTiles />
     <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
     <Testimonial topDivider />
