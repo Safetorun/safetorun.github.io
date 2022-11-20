0bec6756a66eb40e5b7d7cf85a9396f1c1bb33c3
dllewellyn <danieln.llewellyn@gmail.com>
2022-11-20 10:46:02 +0000

    Adding some SEO things

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,21 +5,32 @@ import FeaturesTiles from '../components/sections/FeaturesTiles';
 import FeaturesSplit from '../components/sections/FeaturesSplit';
 //import Testimonial from '../components/sections/Testimonial';
 import Cta from '../components/sections/Cta';
+import {Helmet} from "react-helmet";
+
+const SEO = () => {
+    return (<Helmet>
+        <title>Safe to run - Android security for developers</title>
+        <meta name="description"
+              content="Safe to run provides an API for security as code to make Android application security simple"/>
+        <meta name="keywords"
+              content="android security,security as code,security,android,devsecops,mobile,mobile security,"/>
+    </Helmet>)
+}
 
 const Home = () => {
 
-  return (
-    <>
-      <Hero className="illustration-section-01" />
-        <FeaturesTiles />
-    <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
-    {/**
-    <Testimonial topDivider />
+    return (<>
+            <SEO/>
+            <Hero className="illustration-section-01"/>
+            <FeaturesTiles/>
+            <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02"/>
+            {/**
+             <Testimonial topDivider />
 
-    */}
-    <Cta split />
-    </>
-  );
+             */}
+            <Cta split/>
+        </>
+    );
 }
 
 export default Home;
