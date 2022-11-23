632956e34217e244c870cb3b06b7a9b47136fe5c
dllewellyn <danieln.llewellyn@gmail.com>
2022-11-23 07:44:53 +0000

    Add analytics event

--- src/SlackRedirect.js
+++ src/SlackRedirect.js
@@ -1,7 +1,17 @@
 import React, {useEffect} from 'react';
+import ReactGA from 'react-ga4';
 
 const SlackRedirect = () => {
     useEffect(() => {
+
+        ReactGA.event({
+            category: 'Slack',
+            action: 'slack_redirect',
+            label: 'slack_redirect',
+            value: 1
+        });
+
+
         window.location.href = "https://join.slack.com/t/safetorun/shared_invite/zt-1kfcp4cw0-Cr_BxI5AG~LOEpnM39NIhw";
         }, []);
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import {HashRouter} from "react-router-dom";
+import {createBrowserHistory} from "history";
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -17,10 +17,10 @@ history.listen((location) => {
 });
 
 ReactDOM.render(
-        <HashRouter history={history}>
-    <App />
-        </HashRouter>,
-  document.getElementById('root')
+    <HashRouter history={history}>
+        <App/>
+    </HashRouter>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
