19a641932860dd627475d516eb31c3ddd39823ad
dllewellyn <danieln.llewellyn@gmail.com>
2023-07-15 07:03:11 +0100

    Clean up of tiles into multiple classes

--- src/views/Home.js
+++ src/views/Home.js
@@ -8,6 +8,8 @@ import Cta from '../components/sections/Cta';
 import {Helmet} from "react-helmet";
 import {sectionHeader1, tilesData} from "./safetorun_features";
 import {sectionHeader2, tilesData2} from "./safetorun_plus_features";
+import Button from "../components/elements/Button";
+import {redirectUrl} from "../utils/RedirectUrl";
 
 const SEO = () => {
     return (<Helmet>
@@ -27,14 +29,15 @@ const Home = () => {
             <Hero className="illustration-section-01"/>
             <FeaturesTiles sectionHeader={sectionHeader1} tilesData={tilesData}/>
             <FeaturesTiles sectionHeader={sectionHeader2} tilesData={tilesData2}/>
-            {/*<section>*/}
-            {/*    <div className="illustration-section-02">*/}
-            {/*        <Button tag="button" color="primary" wideMobile*/}
-            {/*                onClick={() => redirectUrl("https://app.safetorun.com?utm_source=landing_page&utm_medium=get_started_button&utm_campaign=default", "click_app_redirect")}>*/}
-            {/*            Plus*/}
-            {/*        </Button>*/}
-            {/*    </div>*/}
-            {/*</section>*/}
+            <section>
+                <div className="illustration-section-02" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <Button tag="button" color="primary" wideMobile
+                            onClick={() => redirectUrl("https://app.safetorun.com?utm_source=landing_page&utm_medium=after_spiel_button&utm_campaign=default", "click_app_redirect")}>
+                        Get started with Plus (It's free)
+                    </Button>
+                </div>
+            </section>
+
             <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02"/>
             {/**
              <Testimonial topDivider />
